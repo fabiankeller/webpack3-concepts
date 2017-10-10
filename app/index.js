6ffc5d7de6bbb538119cb011bc81a43f9670b20a
Fabian Keller (taakefa2) <fabian.keller@swisscom.com>
2017-10-10 15:29:25 +0200

    change content to webpack 3

--- app/index.js
+++ app/index.js
@@ -1,23 +1,18 @@
-(function(){
-  "use strict";
+(function () {
+    "use strict";
 
-  // include index.html for webpack to load
-  require("./index.html");
+    // include index.html for webpack to load
+    require("./index.html");
 
-  var Reveal = require("reveal.js");
-  require("reveal.js/css/reveal.css");
-  // uncomment theme you want to load below
-  require("reveal.js/css/theme/night.css");
+    var Reveal = require("reveal.js");
+    require("reveal.js/css/reveal.css");
+    // uncomment theme you want to load below
+    require("reveal.js/css/theme/white.css");
 
 
-  Reveal.initialize({
-    history: true,
-    // More info https://github.com/hakimel/reveal.js#dependencies
-   /* dependencies: [
-      { src: 'plugin/markdown/marked.js' },
-      { src: 'plugin/markdown/markdown.js' },
-      { src: 'plugin/notes/notes.js', async: true },
-      { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-    ]*/
-  });
+    Reveal.initialize({
+        history: true,
+        // More info https://github.com/hakimel/reveal.js#dependencies
+        dependencies: []
+    });
 })();
